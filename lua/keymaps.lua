a35df7246a34694424cdb42e954993de96f6eaa1
qff233 <347633553@qq.com>
2023-07-10 12:20:45 +0800

    修改键盘映射代码为表格配置

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,42 +1,44 @@
 vim.g.mapleader = " "
+local keymap = {
+    {from = "<c-h>", to = "<Left>", mode = "i"},
+    {from = "<c-i>", to = "<Down>", mode = "i"},
+    {from = "<c-k>", to = "<Up>", mode = "i"},
+    {from = "<c-l>", to = "<Right>", mode = "i"},
 
-local keymap = vim.keymap
+    {from = "<leader>q", to = "<cmd> q <cr>"},
+    {from = "<leader>ww", to = "<cmd> w <cr>"},
+    {from = "<leader>wq", to = "<cmd> wq <cr>"},
+    {from = "<Esc>", to = "<cmd> noh <cr>"},
 
-keymap.set("i", "<c-h>", "<Left>")
-keymap.set("i", "<c-j>", "<Down>")
-keymap.set("i", "<c-k>", "<Up>")
-keymap.set("i", "<c-l>", "<Right>")
+    {from = "<leader>e", to = "<cmd> NvimTreeToggle <cr>"},
+    {from = "<leader>tl", to = "<cmd> TroubleToggle workspace_diagnostics <cr>"},
+    
+    -- telescope
+    {from = "<leader>ff", to = "<cmd>Telescope find_files<cr>"},
+    {from = "<leader>fs", to = "<cmd>Telescope live_grep<cr>"},
+    {from = "<leader>fc", to = "<cmd>Telescope grep_string<cr>"},
+    {from = "<leader>fb", to = "<cmd>Telescope buffers<cr>"},
+    {from = "<leader>fh", to = "<cmd>Telescope help_tags<cr>"},
+    {from = "<leader>lds", to = "<cmd>Telescope lsp_document_symbols<cr>"},
 
-keymap.set("n", "<leader>q", "<cmd> q <cr>")
-keymap.set("n", "<leader>ww", "<cmd> w <cr>")
-keymap.set("n", "<leader>wq", "<cmd> wq <cr>")
-keymap.set("n", "<Esc>", "<cmd> noh <cr>")
+    -- telescope git
+    {from = "<leader>gc", to = "<cmd>Telescope git_commits<cr>"},   -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+    {from = "<leader>gb", to = "<cmd>Telescope git_branches<cr>"},  -- list git branches (use <cr> to checkout) ["gb" for git branch]
+    {from = "<leader>gs", to = "<cmd>Telescope git_status<cr>"},    -- list current changes per file with diff preview ["gs" for git status]
 
-keymap.set("n", "<leader>e", "<cmd> NvimTreeToggle <cr>")
-keymap.set("n", "<leader>tl", "<cmd> TroubleToggle workspace_diagnostics <cr>")
+    --buffer line
+    {from = "H", to = "<cmd> bprevious <cr>"},
+    {from = "L", to = "<cmd> bnext<cr>"},
+    {from = "X", to = "<cmd> bdel <cr> <cmd> bnext <cr>"},
 
--- telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
-keymap.set("n", "<leader>lds", "<cmd>Telescope lsp_document_symbols<cr>") -- list all functions/structs/classes/modules in the current buffers
+    -- terminal
+    {from = "<A-h>", to = function() require("nvterm.terminal").toggle("horizontal") end},
+    {from = "<A-v>", to = function() require("nvterm.terminal").toggle("vertical") end},
+    {from = "<C-x>", to = "<C-\\><C-N>", mode = "t"},
+}
 
--- telescope git commands
-keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
-keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
-keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+for i=1,#keymap do
+    local config = keymap[i]
+    vim.keymap.set(config.mode or "n", config.from, config.to)
+end
 
-keymap.set("n", "H", "<cmd> bprevious <cr>")
-keymap.set("n", "L", "<cmd> bnext <cr>")
-keymap.set("n", "X", "<cmd> bdel <cr>")
-
-keymap.set("n", "<A-h>", function()
-	require("nvterm.terminal").toggle("horizontal")
-end)
-keymap.set("n", "<A-v>", function()
-	require("nvterm.terminal").toggle("vertical")
-end)
-
-keymap.set("t", "<C-x>", "<C-\\><C-N>")
