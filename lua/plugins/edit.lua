2092969c7b331134981216cb39c66653540df9fe
qff233 <347633553@qq.com>
2023-07-16 18:31:05 +0800

    添加flash.nvim跳转插件，删除不要的yml配置文件

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -1,49 +1,57 @@
 return {
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		opts = function()
-			return require("plugins.configs.others").indent_blankline
-		end,
-	},
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        opts = function()
+            return require("plugins.configs.others").indent_blankline
+        end,
+    },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = function()
-			return require("plugins.configs.treesitter")
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function()
+            return require("plugins.configs.treesitter")
+        end,
+    },
 
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = function()
-			return require("plugins.configs.others").gitsigns
-		end,
-	},
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = function()
+            return require("plugins.configs.others").gitsigns
+        end,
+    },
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		opts = function()
-			return require("plugins.configs.nvimtree")
-		end,
-		config = function(_, opts)
-			require("nvim-tree").setup(opts)
-			vim.g.nvimtree_side = opts.view.side
-		end,
-	},
+    {
+        "nvim-tree/nvim-tree.lua",
+        opts = function()
+            return require("plugins.configs.nvimtree")
+        end,
+        config = function(_, opts)
+            require("nvim-tree").setup(opts)
+            vim.g.nvimtree_side = opts.view.side
+        end,
+    },
 
-	{
-		"RRethy/vim-illuminate",
-		event = { "BufReadPost", "BufNewFile" },
-		opts = {
-			delay = 200,
-			large_file_cutoff = 2000,
-			large_file_overrides = {
-				providers = { "lsp" },
-			},
-		},
-		config = function(_, opts)
-			require("illuminate").configure(opts)
-		end,
-	},
+    {
+        "RRethy/vim-illuminate",
+        event = { "BufReadPost", "BufNewFile" },
+        opts = {
+            delay = 200,
+            large_file_cutoff = 2000,
+            large_file_overrides = {
+                providers = { "lsp" },
+            },
+        },
+        config = function(_, opts)
+            require("illuminate").configure(opts)
+        end,
+    },
 
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end },
+        }
+    }
 }
