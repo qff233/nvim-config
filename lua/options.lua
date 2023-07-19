68cc5a48fa45917e93dbdbd46db0eb178eac3a63
qff233 <347633553@qq.com>
2023-07-19 17:39:27 +0800

    添加自动保存、重新打开恢复光标位置、自动折叠c、cpp

--- lua/options.lua
+++ lua/options.lua
@@ -1,39 +1,66 @@
 local opt = {
-    number = true,
+	number = true,
 
-    tabstop = 4,
-    shiftwidth = 4,
-    expandtab = true,
-    autoindent = true,
+	tabstop = 4,
+	shiftwidth = 4,
+	expandtab = true,
+	autoindent = true,
 
-    scrolloff = 15,
-    cursorline = true,
-    wrap = false,
-    clipboard = function(clip) clip:append("unnamedplus") end,
+	scrolloff = 15,
+	cursorline = true,
+	wrap = false,
+	clipboard = function(clip)
+		clip:append("unnamedplus")
+	end,
 
-    splitright = true,
-    splitbelow = true,
+	splitright = true,
+	splitbelow = true,
 
-    ignorecase = true,
-    smartcase = true,
+	ignorecase = true,
+	smartcase = true,
 
-    termguicolors = true,
-    signcolumn = "yes",
+	termguicolors = true,
+	signcolumn = "yes",
 }
 
 for key, value in pairs(opt) do
-    if type(value) == "function" then
-        value(vim.opt[key])
-    else
-        vim.opt[key] = value
-    end
+	if type(value) == "function" then
+		value(vim.opt[key])
+	else
+		vim.opt[key] = value
+	end
 end
 
+-- 自动保存
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+	pattern = { "*" },
+	command = "silent! wall",
+	nested = true,
+})
+
+-- 重新打开恢复光标位置
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = "*",
+	callback = function()
+		if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+			vim.fn.setpos(".", vim.fn.getpos("'\""))
+			vim.cmd("silent! foldopen")
+		end
+	end,
+})
+
+-- 打开C、C++文件自动折叠
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = { "*.c", "*.cpp" },
+	command = "setlocal foldmethod=syntax",
+})
+
+-- 复制高亮
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-    pattern = { "*" },
-    callback = function()
-        vim.highlight.on_yank({
-            timeout = 300,
-        })
-    end
+	pattern = { "*" },
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 300,
+		})
+	end,
 })
