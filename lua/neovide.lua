7364c05a06b6dd4c0de899597e2ed906a805ba1c
qff233 <347633553@qq.com>
2024-06-21 12:19:22 +0800

    修改主题和添加neovide配置

---
+++ lua/neovide.lua
@@ -0,0 +1,14 @@
+vim.o.guifont = "Hack Nerd Font Mono:h14"
+vim.g.neovide_transparency = 0.9
+
+vim.g.neovide_refresh_rate = 60
+vim.g.neovide_refresh_rate_idle = 5
+vim.g.neovide_no_idle = false
+
+vim.g.neovide_remember_window_size = true
+
+vim.g.neovide_cursor_animation_length = 0.05
+vim.g.neovide_cursor_trail_size = 0.8
+vim.g.neovide_cursor_antialiasing = true
+vim.g.neovide_cursor_animate_in_insert_mode = false
+vim.g.neovide_cursor_animate_command_line = true
