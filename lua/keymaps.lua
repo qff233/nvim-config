807e45655e40fd18a4f57db91985a7f91d3c6fa5
qff233 <347633553@qq.com>
2024-10-08 00:22:47 +0800

    修正补全bug，替换不更新的插件

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,56 +2,54 @@ local M = {}
 
 vim.g.mapleader = " "
 M.nvim_tree = {
-    { "<leader>e",   "<cmd> NvimTreeToggle <cr>" },
+	{ "<leader>e", "<cmd> NvimTreeToggle <cr>" },
 }
 
 M.trouble = {
-    { "<leader>xx",  "<cmd> TroubleToggle workspace_diagnostics <cr>" },
+	{ "<leader>xx", "<cmd> TroubleToggle workspace_diagnostics <cr>" },
 }
 
 local keymap = {
-    { "<leader>q",   "<cmd> q <cr>" },
-    { "<leader>ww",  "<cmd> w <cr>" },
-    { "<leader>wq",  "<cmd> wq <cr>" },
-    { "<Esc>",       "<cmd> noh <cr>" },
+	{ "<leader>q", "<cmd> q <cr>" },
+	{ "<leader>ww", "<cmd> w <cr>" },
+	{ "<leader>wq", "<cmd> wq <cr>" },
+	{ "<Esc>", "<cmd> noh <cr>" },
 
+	-- telescope
+	{ "<leader>ff", "<cmd>Telescope find_files<cr>" },
+	{ "<leader>fb", "<cmd>Telescope buffers<cr>" },
+	{ "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+	{ "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>" },
 
-    -- telescope
-    { "<leader>ff",  "<cmd>Telescope find_files<cr>" },
-    { "<leader>fs",  "<cmd>Telescope live_grep<cr>" },
-    { "<leader>fc",  "<cmd>Telescope grep_string<cr>" },
-    { "<leader>fb",  "<cmd>Telescope buffers<cr>" },
-    { "<leader>lds", "<cmd>Telescope lsp_document_symbols<cr>" },
+	-- telescope git
+	{ "<leader>gc", "<cmd>Telescope git_commits<cr>" }, -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+	{ "<leader>gb", "<cmd>Telescope git_branches<cr>" }, -- list git branches (use <cr> to checkout) ["gb" for git branch]
+	{ "<leader>gs", "<cmd>Telescope git_status<cr>" }, -- list current changes per file with diff preview ["gs" for git status]
 
-    -- telescope git
-    { "<leader>gc",  "<cmd>Telescope git_commits<cr>" },  -- list all git commits (use <cr> to checkout) ["gc" for git commits]
-    { "<leader>gb",  "<cmd>Telescope git_branches<cr>" }, -- list git branches (use <cr> to checkout) ["gb" for git branch]
-    { "<leader>gs",  "<cmd>Telescope git_status<cr>" },   -- list current changes per file with diff preview ["gs" for git status]
-
-    --buffer line
-    { "te",          "<cmd> :tabedit <cr>" },
-    { "<tab>",       "<cmd> :tabnext <cr>" },
-    { "<s-tab>",     "<cmd> :tabprev <cr>" },
-    { "td",          "<cmd> bdel <cr>" },
+	--buffer line
+	{ "te", "<cmd> :tabedit <cr>" },
+	{ "<tab>", "<cmd> :tabnext <cr>" },
+	{ "<s-tab>", "<cmd> :tabprev <cr>" },
+	{ "td", "<cmd> bdel <cr>" },
 }
 
 M.lsp_keymaps = {
-    { from = "gD",         to = vim.lsp.buf.declaration },
-    { from = "gd",         to = "<cmd>Lspsaga peek_definition<cr>" },
-    { from = "K",          to = "<cmd>Lspsaga hover_doc<cr>" },
-    { from = "gi",         to = vim.lsp.buf.implementation },
-    { from = "<C-k>",      to = vim.lsp.buf.signature_help },
-    { from = "<leader>D",  to = "<cmd>Lspsaga show_line_diagnositcs<cr>" },
-    { from = "<leader>rn", to = "<cmd>Lspsaga rename<cr>" },
-    { from = "<leader>ca", to = "<cmd>Lspsaga code_action<cr>" },
-    { from = "gr",         to = "<cmd>Lspsaga lsp_finder<cr>" },
-    { from = "[d",         to = "<cmd>Lspsaga diagnostic_jump_prev<cr>" },
-    { from = "]d",         to = "<cmd>Lspsaga diagnostic_jump_next<cr>" },
+	{ from = "gD", to = vim.lsp.buf.declaration },
+	{ from = "gd", to = "<cmd>Lspsaga peek_definition<cr>" },
+	{ from = "K", to = "<cmd>Lspsaga hover_doc<cr>" },
+	{ from = "gi", to = vim.lsp.buf.implementation },
+	{ from = "<C-k>", to = vim.lsp.buf.signature_help },
+	{ from = "<leader>D", to = "<cmd>Lspsaga show_line_diagnositcs<cr>" },
+	{ from = "<leader>rn", to = "<cmd>Lspsaga rename<cr>" },
+	{ from = "<leader>ca", to = "<cmd>Lspsaga code_action<cr>" },
+	{ from = "gr", to = "<cmd>Lspsaga lsp_finder<cr>" },
+	{ from = "[d", to = "<cmd>Lspsaga diagnostic_jump_prev<cr>" },
+	{ from = "]d", to = "<cmd>Lspsaga diagnostic_jump_next<cr>" },
 }
 
 for i = 1, #keymap do
-    local config = keymap[i]
-    vim.keymap.set(config.mode or "n", config[1], config[2])
+	local config = keymap[i]
+	vim.keymap.set(config.mode or "n", config[1], config[2])
 end
 
 return M
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,242 +1,359 @@
 local keymaps = require("keymaps")
 
 return {
-    {
-        "williamboman/mason.nvim",
-        opts = function()
-            return {
-                ui = {
-                    icons = {
-                        package_pending = " ",
-                        package_installed = "󰄳 ",
-                        package_uninstalled = " 󰚌",
-                    },
-                },
+	{
+		"williamboman/mason.nvim",
+		opts = function()
+			return {
+				ui = {
+					icons = {
+						package_pending = " ",
+						package_installed = "󰄳 ",
+						package_uninstalled = " 󰚌",
+					},
+				},
 
-                max_concurrent_installers = 10,
-            }
-        end,
-    },
+				max_concurrent_installers = 10,
+			}
+		end,
+	},
 
-    {
-        "glepnir/lspsaga.nvim",
-        event = "LspAttach",
-        config = true,
-    },
+	{
+		"glepnir/lspsaga.nvim",
+		event = "LspAttach",
+		config = true,
+	},
 
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require("configs.lspconfig")
-        end,
-    },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-    {
-        "nvimdev/guard.nvim",
-        dependencies = {
-            "nvimdev/guard-collection",
-        },
-        config = function()
-            local ft = require("guard.filetype")
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = { "mason.nvim" },
+		opts = function()
+			local nls = require("null-ls")
+			return {
+				root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+				sources = {
+					nls.builtins.formatting.fish_indent,
+					nls.builtins.diagnostics.fish,
+					nls.builtins.formatting.stylua,
+					nls.builtins.formatting.shfmt,
+					nls.builtins.diagnostics.flake8,
+				},
+			}
+		end,
+	},
 
-            ft("c"):fmt("clang-format")
-            ft("cpp"):fmt("clang-format")
-            ft("typescript,javascript,typescriptreact"):fmt("prettier")
-            ft("lua"):fmt("lsp"):append("stylua")
-            ft("sh"):fmt("shfmt")
-            ft("rust"):fmt("lsp")
-            -- ft("python"):fmt("black"):append("isort")
+	{
+		-- snippet plugin
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
+		},
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+		},
+	},
 
-            vim.g.guardi_config = {
-                -- the only options for the setup function
-                fmt_on_save = true,
-                -- Use lsp if no formatter was defined for this filetype
-                lsp_as_default_formatter = true,
-            }
-        end,
-    },
+	{
+		"rafamadriz/friendly-snippets",
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
 
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            {
-                -- snippet plugin
-                "L3MON4D3/LuaSnip",
-                dependencies = "rafamadriz/friendly-snippets",
-                opts = { history = true, delete_check_events = "TextChanged" },
-            },
+	{
+		"echasnovski/mini.pairs",
+		event = "VeryLazy",
+		config = function(_, opts)
+			require("mini.pairs").setup(opts)
+		end,
+	},
 
-            -- autopairing of (){}[] etc
-            {
-                "windwp/nvim-autopairs",
-                opts = {
-                    fast_wrap = {},
-                    disable_filetype = { "TelescopePrompt", "vim" },
-                },
-                config = function(_, opts)
-                    require("nvim-autopairs").setup(opts)
+	{
+		"echasnovski/mini.surround",
+		keys = function(_, keys)
+			-- Populate the keys based on the user's options
+			local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
+			local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+			local mappings = {
+				{ opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
+				{ opts.mappings.delete, desc = "Delete surrounding" },
+				{ opts.mappings.find, desc = "Find right surrounding" },
+				{ opts.mappings.find_left, desc = "Find left surrounding" },
+				{ opts.mappings.highlight, desc = "Highlight surrounding" },
+				{ opts.mappings.replace, desc = "Replace surrounding" },
+				{ opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+			}
+			mappings = vim.tbl_filter(function(m)
+				return m[1] and #m[1] > 0
+			end, mappings)
+			return vim.list_extend(mappings, keys)
+		end,
+		opts = {
+			mappings = {
+				add = "gza", -- Add surrounding in Normal and Visual modes
+				delete = "gzd", -- Delete surrounding
+				find = "gzf", -- Find surrounding (to the right)
+				find_left = "gzF", -- Find surrounding (to the left)
+				highlight = "gzh", -- Highlight surrounding
+				replace = "gzr", -- Replace surrounding
+				update_n_lines = "gzn", -- Update `n_lines`
+			},
+		},
+		config = function(_, opts)
+			-- use gz mappings instead of s to prevent conflict with leap
+			require("mini.surround").setup(opts)
+		end,
+	},
 
-                    -- setup cmp for autopairs
-                    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-                    require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-                end,
-            },
+	{
+		"echasnovski/mini.surround",
+		keys = function(_, keys)
+			-- Populate the keys based on the user's options
+			local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
+			local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+			local mappings = {
+				{ opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
+				{ opts.mappings.delete, desc = "Delete surrounding" },
+				{ opts.mappings.find, desc = "Find right surrounding" },
+				{ opts.mappings.find_left, desc = "Find left surrounding" },
+				{ opts.mappings.highlight, desc = "Highlight surrounding" },
+				{ opts.mappings.replace, desc = "Replace surrounding" },
+				{ opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+			}
+			mappings = vim.tbl_filter(function(m)
+				return m[1] and #m[1] > 0
+			end, mappings)
+			return vim.list_extend(mappings, keys)
+		end,
+		opts = {
+			mappings = {
+				add = "gza", -- Add surrounding in Normal and Visual modes
+				delete = "gzd", -- Delete surrounding
+				find = "gzf", -- Find surrounding (to the right)
+				find_left = "gzF", -- Find surrounding (to the left)
+				highlight = "gzh", -- Highlight surrounding
+				replace = "gzr", -- Replace surrounding
+				update_n_lines = "gzn", -- Update `n_lines`
+			},
+		},
+		config = function(_, opts)
+			-- use gz mappings instead of s to prevent conflict with leap
+			require("mini.surround").setup(opts)
+		end,
+	},
 
-            -- cmp sources plugins
-            {
-                "saadparwaiz1/cmp_luasnip",
-                "hrsh7th/cmp-nvim-lua",
-                "hrsh7th/cmp-nvim-lsp",
-                "hrsh7th/cmp-buffer",
-                "hrsh7th/cmp-path",
-                "onsails/lspkind.nvim",
-            },
-        },
-        opts = function()
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
-            local lspkind = require("lspkind")
-            local function has_words_before()
-                unpack = unpack or table.unpack
-                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                return col ~= 0
-                    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-            end
-            return {
-                completion = {
-                    completeopt = "menu,menuone,noselect",
-                },
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"onsails/lspkind.nvim",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		opts = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
+			return {
+				completion = {
+					completeopt = "menu,menuone,noselect",
+				},
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = {
+					["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					["<S-CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = false,
+					}),
+				},
+				-- window = {
+				-- 	completion = {
+				-- 		winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel",
+				-- 		scrollbar = true,
+				-- 	},
+				-- 	documentation = {
+				-- 		winhighlight = "Normal:CmpDoc",
+				-- 	},
+				-- },
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+					{ name = "nvim_lua" },
+				},
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol",
+						maxwidth = 50,
+						ellipsis_char = "...",
+					}),
+				},
+			}
+		end,
+	},
 
-                window = {
-                    completion = {
-                        winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel",
-                        scrollbar = true,
-                    },
-                    documentation = {
-                        winhighlight = "Normal:CmpDoc",
-                    },
-                },
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                formatting = {
-                    format = lspkind.cmp_format({
-                        mode = "symbol",
-                        maxwidth = 50,
-                        ellipsis_char = "...",
-                    }),
-                },
-                mapping = {
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        elseif has_words_before() then
-                            cmp.complete()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<C-u>"] = cmp.mapping.scroll_docs(-5),
-                    ["<C-d>"] = cmp.mapping.scroll_docs(5),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.close(),
-                    ["<CR>"] = cmp.mapping({
-                        i = function(fallback)
-                            if cmp.visible() and cmp.get_active_entry() then
-                                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-                            else
-                                fallback()
-                            end
-                        end,
-                        s = cmp.mapping.confirm({ select = true }),
-                        c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-                    }),
-                },
-                sources = {
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "buffer" },
-                    { name = "nvim_lua" },
-                    { name = "path" },
-                },
-            }
-        end,
-    },
+	{
+		"echasnovski/mini.comment",
+		event = "VeryLazy",
+		--         opts = {
+		--             hooks = {
+		--                 pre = function()
+		--                     require("ts_context_commentstring.internal").update_commentstring({})
+		--                 end,
+		--             },
+		--         },
+		config = function(_, opts)
+			require("mini.comment").setup(opts)
+		end,
+	},
 
-    {
-        "numToStr/Comment.nvim",
-        config = function(_, opts)
-            require("Comment").setup(opts)
-        end,
-    },
+	{
+		"echasnovski/mini.ai",
+		-- keys = {
+		--   { "a", mode = { "x", "o" } },
+		--   { "i", mode = { "x", "o" } },
+		-- },
+		event = "VeryLazy",
+		dependencies = { "nvim-treesitter-textobjects" },
+		opts = function()
+			local ai = require("mini.ai")
+			return {
+				n_lines = 500,
+				custom_textobjects = {
+					o = ai.gen_spec.treesitter({
+						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+					}, {}),
+					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+				},
+			}
+		end,
+		config = function(_, opts)
+			require("mini.ai").setup(opts)
+			-- register all text objects with which-key
+			---@type table<string, string|table>
+			local i = {
+				[" "] = "Whitespace",
+				['"'] = 'Balanced "',
+				["'"] = "Balanced '",
+				["`"] = "Balanced `",
+				["("] = "Balanced (",
+				[")"] = "Balanced ) including white-space",
+				[">"] = "Balanced > including white-space",
+				["<lt>"] = "Balanced <",
+				["]"] = "Balanced ] including white-space",
+				["["] = "Balanced [",
+				["}"] = "Balanced } including white-space",
+				["{"] = "Balanced {",
+				["?"] = "User Prompt",
+				_ = "Underscore",
+				a = "Argument",
+				b = "Balanced ), ], }",
+				c = "Class",
+				f = "Function",
+				o = "Block, conditional, loop",
+				q = "Quote `, \", '",
+				t = "Tag",
+			}
+			local a = vim.deepcopy(i)
+			for k, v in pairs(a) do
+				a[k] = v:gsub(" including.*", "")
+			end
 
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = { icons = false },
-        keys = keymaps.trouble
-    },
+			local ic = vim.deepcopy(i)
+			local ac = vim.deepcopy(a)
+			for key, name in pairs({ n = "Next", l = "Last" }) do
+				i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, ic)
+				a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, ac)
+			end
+			-- require("which-key").register({
+			--     mode = { "o", "x" },
+			--     i = i,
+			--     a = a,
+			-- })
+		end,
+	},
 
-    {
-        "j-hui/fidget.nvim",
-        requires = { "neovim/nvim-lspconfig" },
-        tag = "legacy",
-        config = true,
-    },
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = { icons = false },
+		keys = keymaps.trouble,
+	},
 
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        -- opts = function()
-        --     return {
-        --         show_current_context = true,
-        --     }
-        -- end,
-    },
+	{
+		"j-hui/fidget.nvim",
+		requires = { "neovim/nvim-lspconfig" },
+		tag = "legacy",
+		config = true,
+	},
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = function()
-            return {
-                ensure_installed = {
-                    "bash",
-                    "rust",
-                    "cpp",
-                    "c",
-                    "html",
-                    "javascript",
-                    "json",
-                    "lua",
-                    "markdown",
-                    "markdown_inline",
-                    "python",
-                    "typescript",
-                    "yaml",
-                },
-                sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-            }
-        end,
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup(opts)
-        end,
-    },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		-- opts = function()
+		--     return {
+		--         show_current_context = true,
+		--     }
+		-- end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function()
+			return {
+				ensure_installed = {
+					"bash",
+					"rust",
+					"cpp",
+					"c",
+					"html",
+					"javascript",
+					"json",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"typescript",
+					"yaml",
+				},
+				sync_install = false,
+				auto_install = true,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+			}
+		end,
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
 }
