b41aa11d219063f21d007929d47cff21709c233b
qff233 <347633553@qq.com>
2024-10-08 23:26:54 +0800

    修改补全方式为tab

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,32 +1,31 @@
 return {
-    {
-        "nvim-lualine/lualine.nvim",
-        opts = function()
-            return {
-                theme = "everforest"
-            }
-        end,
-    },
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = function()
+			return {
+				theme = "everforest",
+			}
+		end,
+	},
 
-    {
-        "akinsho/bufferline.nvim",
-        opts = {
-            options = {
-                mode = "tabs",
-                diagnostics = "nvim_lsp",
-                offsets = {
-                    {
-                        filetype = "NvimTree",
-                        text = "File Explorer",
-                        highlight = "Directory",
-                        separator = true
-                    }
-                },
-                always_show_bufferline = false,
-                show_buffer_close_icons = false,
-                show_close_icon = false,
-            },
-        }
-        ,
-    },
+	{
+		"akinsho/bufferline.nvim",
+		opts = {
+			options = {
+				mode = "tabs",
+				diagnostics = "nvim_lsp",
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						highlight = "Directory",
+						separator = true,
+					},
+				},
+				always_show_bufferline = false,
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+			},
+		},
+	},
 }
