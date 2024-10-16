881ce9aae381f66b7ea3a7cdca858cd9e071004e
qff233 <347633553@qq.com>
2024-10-16 17:38:36 +0800

    change: 自动折叠行号修改为200,开启neoconf插件

--- lua/options.lua
+++ lua/options.lua
@@ -1,56 +1,56 @@
 local opt = {
-    showmode = false,
-    number = true,
+	showmode = false,
+	number = true,
 
-    tabstop = 4,
-    shiftwidth = 4,
-    expandtab = true,
-    autoindent = true,
+	tabstop = 4,
+	shiftwidth = 4,
+	expandtab = true,
+	autoindent = true,
 
-    scrolloff = 10,
-    cursorline = true,
-    wrap = false,
-    clipboard = function(clip)
-        clip:append("unnamedplus")
-    end,
+	scrolloff = 10,
+	cursorline = true,
+	wrap = false,
+	clipboard = function(clip)
+		clip:append("unnamedplus")
+	end,
 
-    splitright = true,
-    splitbelow = true,
+	splitright = true,
+	splitbelow = true,
 
-    -- search
-    ignorecase = true,
-    smartcase = true,
+	-- search
+	ignorecase = true,
+	smartcase = true,
 
-    termguicolors = true,
-    signcolumn = "yes",
+	termguicolors = true,
+	signcolumn = "yes",
 
-    autoread = true,
+	autoread = true,
 
-    title = true,
+	title = true,
 
-    swapfile = false,
-    backup = false,
+	swapfile = false,
+	backup = false,
 
-    updatetime = 50,
+	updatetime = 50,
 
-    mouse = "a",
+	mouse = "a",
 
-    undofile = true,
-    undodir = vim.fn.expand("$HOME/.local/share/nvim/undo"),
+	undofile = true,
+	undodir = vim.fn.expand("$HOME/.local/share/nvim/undo"),
 
-    exrc = true,
+	exrc = true,
 
-    foldmethod = "expr",
-    foldexpr = "v:lua.vim.treesitter.foldexpr()",
-    foldlevelstart = 99
+	foldmethod = "expr",
+	foldexpr = "v:lua.vim.treesitter.foldexpr()",
+	foldlevelstart = 200,
 }
 
 for key, value in pairs(opt) do
-    if type(value) == "function" then
-        value(vim.opt[key])
-    else
-        vim.opt[key] = value
-    end
+	if type(value) == "function" then
+		value(vim.opt[key])
+	else
+		vim.opt[key] = value
+	end
 end
 
 -- vim.wo.foldmethod = "expr"
@@ -58,21 +58,21 @@ end
 --     vim.wo.
 -- 重新打开恢复光标位置
 vim.api.nvim_create_autocmd("BufReadPost", {
-    pattern = "*",
-    callback = function()
-        if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
-            vim.fn.setpos(".", vim.fn.getpos("'\""))
-            vim.cmd("silent! foldopen")
-        end
-    end,
+	pattern = "*",
+	callback = function()
+		if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+			vim.fn.setpos(".", vim.fn.getpos("'\""))
+			vim.cmd("silent! foldopen")
+		end
+	end,
 })
 
 -- 复制高亮
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-    pattern = { "*" },
-    callback = function()
-        vim.highlight.on_yank({
-            timeout = 300,
-        })
-    end,
+	pattern = { "*" },
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 300,
+		})
+	end,
 })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,72 +1,72 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        -- commit = "8ab8f4cf48425dcb4845a61d3caa2d2a7e3d9df7",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "folke/neoconf.nvim",
-            "folke/lazydev.nvim",
-            "glepnir/lspsaga.nvim",
-        },
-        config = function()
-            -- require("neoconf").setup()
-            -- require("lazydev").setup()
-            require("lspsaga").setup()
-            require("mason").setup({
-                ui = {
-                    icons = {
-                        package_pending = " ",
-                        package_installed = "󰄳 ",
-                        package_uninstalled = " 󰚌",
-                    },
-                },
+	{
+		"neovim/nvim-lspconfig",
+		-- commit = "8ab8f4cf48425dcb4845a61d3caa2d2a7e3d9df7",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"folke/neoconf.nvim",
+			"folke/lazydev.nvim",
+			"glepnir/lspsaga.nvim",
+		},
+		config = function()
+			-- require("lazydev").setup()
+			require("neoconf").setup()
+			require("lspsaga").setup()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_pending = " ",
+						package_installed = "󰄳 ",
+						package_uninstalled = " 󰚌",
+					},
+				},
 
-                max_concurrent_installers = 10,
-            })
-            local on_attach = function(client, bufnr)
-                local opts = { noremap = false, buffer = bufnr }
-                local keymap = require("keymaps").lsp_keymaps
-                for i = 1, #keymap do
-                    vim.keymap.set("n", keymap[i][1], keymap[i][2], opts)
-                end
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
-                    buffer = bufnr,
-                    callback = function()
-                        vim.lsp.buf.format()
-                    end,
-                })
-                if client.server_capabilities.documentSymbolProvider then
-                    require("nvim-navic").attach(client, bufnr)
-                end
-            end
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            -- local capabilities = vim.lsp.protocol.make_client_capabilities()
-            local servers = require("configs/lsp_config")
-            require("mason-lspconfig").setup({
-                ensure_installed = vim.tbl_keys(servers),
-            })
-            for server, config in pairs(servers) do
-                require("lspconfig")[server].setup(vim.tbl_deep_extend("keep", {
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                }, config))
-            end
-        end,
-    },
-    --
-    -- {
-    -- 	"folke/trouble.nvim",
-    -- 	dependencies = { "nvim-tree/nvim-web-devicons" },
-    -- 	opts = { icons = false },
-    -- 	keys = keymaps.trouble,
-    -- },
+				max_concurrent_installers = 10,
+			})
+			local on_attach = function(client, bufnr)
+				local opts = { noremap = false, buffer = bufnr }
+				local keymap = require("keymaps").lsp_keymaps
+				for i = 1, #keymap do
+					vim.keymap.set("n", keymap[i][1], keymap[i][2], opts)
+				end
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format()
+					end,
+				})
+				if client.server_capabilities.documentSymbolProvider then
+					require("nvim-navic").attach(client, bufnr)
+				end
+			end
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+			local servers = require("configs/lsp_config")
+			require("mason-lspconfig").setup({
+				ensure_installed = vim.tbl_keys(servers),
+			})
+			for server, config in pairs(servers) do
+				require("lspconfig")[server].setup(vim.tbl_deep_extend("keep", {
+					on_attach = on_attach,
+					capabilities = capabilities,
+				}, config))
+			end
+		end,
+	},
+	--
+	-- {
+	-- 	"folke/trouble.nvim",
+	-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- 	opts = { icons = false },
+	-- 	keys = keymaps.trouble,
+	-- },
 
-    {
-        "j-hui/fidget.nvim",
-        requires = { "neovim/nvim-lspconfig" },
-        tag = "legacy",
-        config = true,
-    },
+	{
+		"j-hui/fidget.nvim",
+		requires = { "neovim/nvim-lspconfig" },
+		tag = "legacy",
+		config = true,
+	},
 }
